2ea614f2aa4b75e2cd2f270f28d763447e18f5d5
ThEditor <theditormcpe@gmail.com>
2023-06-08 17:28:16 +0530

    fix: rearranged layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,14 +5,14 @@ import Sidebar from '@/components/layout/sidebar/Sidebar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='text-alt flex h-screen bg-[#1D2123]'>
-      <Sidebar className='w-[5%]' />
-      <div className='flex flex-grow flex-col'>
+    <div className='text-alt flex h-screen flex-col bg-[#1D2123]'>
+      <div className='flex flex-grow'>
+        <Sidebar className='w-[5%]' />
         <div className='flex-grow outline outline-2 outline-blue-500'>
           {children}
         </div>
-        <Player className='h-[10%] outline outline-2 outline-red-500' />
       </div>
+      <Player className='h-[10%] outline outline-2 outline-red-500' />
     </div>
   );
 }
