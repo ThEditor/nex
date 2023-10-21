e53cad9297384d3fa30c8ffccbdac6647ace1472
OneRandomGithubUser <70461479+Anantu05@users.noreply.github.com>
2023-10-21 16:10:03 +0530

    Fixed CSS
    Fixed CSS, known issues: buttons are not directing to corresponding routes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
-  content: ['./src/app/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
       quicksand: ['Quicksand', 'sans-serif'],
