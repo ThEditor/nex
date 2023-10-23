3edced5ff04c4e8c1a5a2c0e6c537367516641f1
OneRandomGithubUser <70461479+Anantu05@users.noreply.github.com>
2023-10-23 16:46:18 +0530

    Fix: Removed SEO page
    Removed SEO page and its references entirely.

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,13 +1,9 @@
 'use client';
 import * as React from 'react';
 
-import Seo from '@/components/Seo';
-
-
 export default function AccountPage() {
   return (
     <>
-      <Seo />
       <main className='flex items-center justify-center'>
         <h1>Account Settings Here</h1>
       </main>
