8a831fa2732ee3ebde6942591a2db3b0df7f470e
OneRandomGithubUser <70461479+OneRandom1509@users.noreply.github.com>
2023-10-27 23:39:07 +0530

    fix: changed search page to server side

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,14 @@
-import { Metadata } from "next";
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title:"Search",
-}
-export default function Search(){
-  return;
+  title: 'Search',
+};
+export default function Search() {
+  return (
+    <>
+      <main className='flex items-center justify-center'>
+        <h1>Searching...</h1>
+      </main>
+    </>
+  );
 }
